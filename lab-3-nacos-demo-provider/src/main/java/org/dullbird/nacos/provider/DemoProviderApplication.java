5efc89ac8fba9b873f2ab498fab73e18204a2ca8
chenyinshi.cys <chenyinshi.cys@yungu.org>
2020-04-05 23:44:28 +0800

    nacos commited

---
+++ lab-3-nacos-demo-provider/src/main/java/org/dullbird/nacos/provider/DemoProviderApplication.java
@@ -0,0 +1,30 @@
+package org.dullbird.nacos.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author user01
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DemoProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoProviderApplication.class, args);
+    }
+
+    @RestController
+    static class TestController {
+
+        @GetMapping("/echo")
+        public String echo(String name) {
+            return "provider:" + name;
+        }
+
+    }
+
+}
