3416ce43b70417f919f29dae445194bdd860d5e0
chenyinshi.cys <chenyinshi.cys@yungu.org>
2020-07-19 10:58:23 +0800

    引用同一个jar的时候的问题解决

---
+++ other/same-jar-import/same-jar-2/src/main/java/org/dullbird/Hello.java
@@ -0,0 +1,12 @@
+package org.dullbird;
+
+/**
+ * @author dullBird
+ * @version 1.0.0
+ * @createTime 2020年07月19日 10:18:00
+ */
+public class Hello {
+    public String sayHello() {
+        return "hello! I'm same-jar-2";
+    }
+}
---
+++ other/same-jar-import/same-jar-main/src/main/java/TestMain.java
@@ -0,0 +1,13 @@
+import org.dullbird.Hello;
+
+/**
+ * @author dullBird
+ * @version 1.0.0
+ * @createTime 2020年07月19日 10:38:00
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        System.out.println(hello.sayHello());
+    }
+}
