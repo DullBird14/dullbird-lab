6b04b4191ea3353942ca6c3c5ca82f1a8c505c77
dullbird <524318257@qq.com>
2020-10-18 23:57:04 +0800

    提交测试代码

--- other/jdk8-demo/src/main/java/org/dullbird/concurrent/CompletableFutureSourceDemo.java
+++ other/jdk8-demo/src/main/java/org/dullbird/concurrent/CompletableFutureSourceDemo.java
@@ -6,6 +6,51 @@ import java.util.concurrent.Executors;
 
 public class CompletableFutureSourceDemo {
     public static void main(String[] args) {
+//        testOne();
+//        testTwo();
+        testThree();
+
+    }
+
+    private static void testThree() {
+
+        CompletableFuture one = CompletableFuture.supplyAsync(() -> {
+            try {
+                //休眠200秒
+                Thread.sleep(5000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("supplyAsync " + Thread.currentThread().getName());
+            return "hello ";
+        });
+//        CompletableFuture one_two = one.thenAccept((s) -> {
+//            System.out.println("one.result:" + s);
+//            System.out.println("one_two");
+//        });
+        CompletableFuture two = CompletableFuture.supplyAsync(() -> {
+            System.out.println("two start....");
+            boolean mark = true;
+            if (mark) {
+                throw new RuntimeException("exception!!!!");
+            }
+            return "hell two";
+        });
+//                .exceptionally((e) -> {
+//                    System.out.println("two exception " + e);
+//
+//                    return "two exception recover!";
+//                });
+
+        CompletableFuture.allOf(one, two).join();
+//        CompletableFuture two_two = two.thenAccept((s) -> {
+//            System.out.println("two.result:" + s);
+//            System.out.println("two_two");
+//        });
+
+    }
+
+    private static void testOne() {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
         CompletableFuture cf = CompletableFuture.supplyAsync(() -> {
@@ -17,13 +62,7 @@ public class CompletableFutureSourceDemo {
             }
             System.out.println("supplyAsync " + Thread.currentThread().getName());
             return "hello ";
-        },executorService).thenAccept(s -> {
-            try {
-                thenApply_test(s + "world");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        }, executorService);
 
         System.out.println(Thread.currentThread().getName());
         while (true) {
@@ -33,6 +72,37 @@ public class CompletableFutureSourceDemo {
             }
         }
     }
+
+    private static void testTwo() {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        CompletableFuture cf = CompletableFuture.supplyAsync(() -> {
+            try {
+                //休眠200秒
+                Thread.sleep(5000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("supplyAsync " + Thread.currentThread().getName());
+            return "hello ";
+        }, executorService)
+                .thenAccept(s -> {
+                    try {
+                        thenApply_test(s + "world");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        System.out.println(Thread.currentThread().getName());
+        while (true) {
+            if (cf.isDone()) {
+                System.out.println("CompletedFuture...isDown");
+                break;
+            }
+        }
+    }
+
 
     private static void thenApply_test(String s) {
         System.out.println(s);
