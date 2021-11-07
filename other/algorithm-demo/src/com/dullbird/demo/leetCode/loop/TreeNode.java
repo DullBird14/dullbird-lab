f674926ed52c0f449b7ce43363245376466791d8
dullbird <524318257@qq.com>
2021-11-07 20:17:11 +0800

    提交刷题代码

--- other/algorithm-demo/src/com/dullbird/demo/leetCode/loop/TreeNode.java
+++ other/algorithm-demo/src/com/dullbird/demo/leetCode/loop/TreeNode.java
@@ -1,5 +1,7 @@
 package com.dullbird.demo.leetCode.loop;
 
+import com.dullbird.demo.linkedlist.ListNode;
+
 /**
  * @author cys
  * @date 2019-11-19 19:41
@@ -9,6 +11,16 @@ public class TreeNode {
 	public int val;
 	public TreeNode left;
 	public TreeNode right;
+
+	public TreeNode() {
+	}
+
+	public TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+
 	public TreeNode(int x) { val = x; }
 
 	@Override
